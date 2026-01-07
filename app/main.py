5902ea025ba5cf46b4e599e4cd2e9c4a4e484d92
AzzraelCode <azzrael@bk.ru>
2026-01-07 14:32:45 +0300

    Очистка acme.json от не используемых сертификатов. Для уменьшения размера acme.json

--- app/main.py
+++ app/main.py
@@ -102,6 +102,76 @@ def generate_dynamic_yml(domains, filename = "dynamic"):
 async def hello():
     return {"hello_to": f"Traefik Dynamic"}
 
+@app.api_route("/clean-acme", methods=["GET"])
+async def clean_acme():
+    """
+    Очищает acme.json от неиспользуемых сертификатов, е домен был удален.
+    Считывает домены из dynamic.yml и local.yml, затем удаляет
+    из acme.json сертификаты для доменов, которые больше не используются.
+    """
+    host_regex = re.compile(r"Host\(`([^`]+)`\)") # Регулярное выражение для извлечения доменов из правил Host(`...`)
+    config_paths = [
+        'dynamic/dynamic.yml',
+        'dynamic/local.yml'
+    ]
+
+    # 1 Собираю текущие активные домены из yml и уникализирую список доменов
+    active_domains = set()
+    for path in config_paths:
+        if not os.path.exists(path): continue
+
+        try:
+            with open(path, 'r', encoding='utf-8') as f:
+                config_data = yaml.safe_load(f)
+                if not config_data or 'http' not in config_data or 'routers' not in config_data['http']: continue
+
+                routers = config_data['http']['routers']
+                for router_name, router_config in routers.items():
+                    if 'rule' in router_config:
+                        found_domains = host_regex.findall(router_config['rule'])
+                        for domain in found_domains:
+                            active_domains.add(domain)
+
+        except Exception as e:
+            print(f"Ошибка при чтении или парсинге файла {path}: {e}")
+
+    if not active_domains: raise HTTPException(status_code=400, detail="Доменов не найдено")
+
+    # 2 Проверка и очистка acme.json
+    acme_path = 'data/acme.json'
+    if not os.path.exists(acme_path): raise HTTPException(status_code=400, detail=f"Файл {acme_path} не найден. Очистка не требуется.")
+
+    try:
+        with open(acme_path, 'r', encoding='utf-8') as f:
+            acme_data = json.load(f)
+
+        # Предполагаем, что используется резолвер с именем 'letsencrypt'
+        resolver_name = 'letsencrypt'
+        if (
+                resolver_name not in acme_data or
+                not isinstance(acme_data[resolver_name].get('Certificates'), list)
+        ):
+            raise HTTPException(status_code=400, detail=f"В файле {acme_path} не найдена корректная структура для резолвера '{resolver_name}'.")
+
+        # сколько доменов в acme.json всего
+        len_original_certs = len(acme_data[resolver_name]['Certificates'])
+
+        # Фильтруем сертификаты, оставляя только те, чей основной домен активен
+        filtered_certs = [cert for cert in acme_data[resolver_name]['Certificates'] if cert.get('domain', {}).get('main') in active_domains]
+        len_filtered_certs = len(filtered_certs)
+
+        # если отфильтрованных столько же, то и удалять нечего
+        if len_filtered_certs >= len_original_certs: raise HTTPException(status_code=400, detail="Неактивные сертификаты в acme.json не найдены. Очистка не требуется.")
+
+        # иначе давайте почистим
+        acme_data[resolver_name]['Certificates'] = filtered_certs
+        with open(acme_path, 'w', encoding='utf-8') as f: json.dump(acme_data, f, indent=2)
+
+        return dict(message=f"Очистка {acme_path} завершена. Удалено {len_original_certs - len_filtered_certs} неактивных сертификатов.")
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Ошибка при обработке файла {acme_path}: {e}")
+
 @app.api_route("/create", methods=["POST", "GET"])
 async def create(request: Request):
     """
